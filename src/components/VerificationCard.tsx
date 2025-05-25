b664e3e8309a37644743f8ff8b3ba99fc743b9fc
boltw282 <wesleydecastrodiass@gmail.com>
2025-05-25 06:30:28 -0300

    Update VerificationCard background with SVG

--- src/components/VerificationCard.tsx
+++ src/components/VerificationCard.tsx
@@ -253,7 +253,7 @@ const VerificationCard: React.FC = () => {
             >
               {isScanning ? (
                 <>
-                  <Loader2 className="w-5 h-5 mr-2 animate-spin" />
+                  <Loader3 className="w-5 h-5 mr-2 animate-spin" />
                   Verificando...
                 </>
               ) : (
