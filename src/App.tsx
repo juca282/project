d60893e7bcce4364ff32279e1d1125da682373bd
magaiver282 <wesleydecastrodiass@gmail.com>
2025-05-26 06:28:44 -0300

    Deploy application to Netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Banner from './components/Banner';
 import VerificationCard from './components/VerificationCard';
@@ -7,21 +6,16 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <Banner />
-          <div className="container mx-auto px-4 -mt-20 relative z-10 mb-12">
-            <Routes>
-              <Route path="/" element={<VerificationCard />} />
-              <Route path="/verificar/:protocolo" element={<VerificationCard />} />
-            </Routes>
-          </div>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        <Banner />
+        <div className="container mx-auto px-4 -mt-20 relative z-10 mb-12">
+          <VerificationCard />
+        </div>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
